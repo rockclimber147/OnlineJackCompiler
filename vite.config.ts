de153ac962145887f3fe64ae0ebd99b8d1f82044
Daylen Smith <daylencsmith@gmail.com>
2026-05-21 18:40:25 -0700

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'OnlineJackCompiler',
+  base: '/OnlineJackCompiler',
   plugins: [
     react(),
     tailwindcss()
